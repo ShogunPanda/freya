20470e0f8df37889305102f2fdd2b838e83ec86b
Paolo Insogna <paolo@cowtech.it>
2024-01-27 17:01:21 +0100

    chore: Removed reference theme.

--- reference/src/themes/main/unocss.config.ts
+++
@@ -1,9 +0,0 @@
-import { type UserConfig } from '@unocss/core'
-import { type Theme as UnoTheme } from '@unocss/preset-mini'
-import { unocssConfig } from '../../../../dist/rendering/unocss.config.js'
-
-const config: UserConfig<UnoTheme> = { ...unocssConfig }
-
-config.layers!.theme = 30
-
-export default config
